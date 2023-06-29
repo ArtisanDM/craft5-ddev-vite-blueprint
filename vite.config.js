f7b18720cf321aa71947bacc76f0ee71b5098e55
Josh Parylak <josh@artisandm.com>
2023-06-29 11:16:33 -0400

    Update packages

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
-import liveReload from "vite-plugin-live-reload";
-import legacy from "@vitejs/plugin-legacy";
-import critical from "rollup-plugin-critical";
-import viteCompression from "vite-plugin-compression";
+import liveReload from 'vite-plugin-live-reload';
+import legacy from '@vitejs/plugin-legacy';
+import critical from 'rollup-plugin-critical';
+import viteCompression from 'vite-plugin-compression';
 
 export default ({ command }) => ({
-	base: command === "serve" ? "" : "/dist/",
+	base: command === 'serve' ? '' : '/dist/',
 	css: { preprocessorOptions: { scss: { charset: false } } },
 	build: {
 		manifest: true,
-		outDir: "./web/dist/",
+		outDir: './web/dist/',
 		rollupOptions: {
 			input: {
-				app: "./src/js/app.ts",
+				app: './src/js/app.ts',
+				css: './src/sass/main.scss',
 			},
 		},
 	},
 	server: {
-		host: "0.0.0.0",
+		host: '0.0.0.0',
 		port: 3000,
 	},
 	plugins: [
-		liveReload(["./templates/**/*"]),
+		liveReload(['./templates/**/*']),
 		legacy({
-			targets: ["defaults"],
-			additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+			targets: ['defaults'],
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
 		}),
 		critical({
-			criticalUrl: "http://localhost",
-			criticalBase: "./web/dist/criticalcss/",
-			criticalPages: [{ uri: "/", template: "index" }],
+			criticalUrl: 'http://localhost',
+			criticalBase: './web/dist/criticalcss/',
+			criticalPages: [{ uri: '/', template: 'index' }],
 			criticalConfig: {},
 		}),
 		viteCompression(),
